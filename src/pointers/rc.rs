9b554dbf0a469c82475de2f34a6411c204ca7479
ahabhgk <ahabhgk@gmail.com>
2021-08-19 17:21:58 +0800

    feat: rc

---
+++ src/pointers/rc.rs
@@ -0,0 +1,60 @@
+use super::cell::Cell;
+use std::{marker::PhantomData, ops::Deref, ptr::NonNull};
+
+struct RcInner<T> {
+    value: T,
+    refcount: Cell<usize>,
+}
+
+pub struct Rc<T> {
+    inner: NonNull<RcInner<T>>,
+    _marker: PhantomData<RcInner<T>>,
+}
+
+impl<T> Rc<T> {
+    pub fn new(value: T) -> Self {
+        let inner = Box::new(RcInner {
+            value,
+            refcount: Cell::new(1),
+        });
+
+        Rc {
+            // SAFETY: Box does not give us a null pointer.
+            inner: unsafe { NonNull::new_unchecked(Box::into_raw(inner)) },
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T> Deref for Rc<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        let inner = unsafe { self.inner.as_ref() };
+        &inner.value
+    }
+}
+
+impl<T> Clone for Rc<T> {
+    fn clone(&self) -> Self {
+        let inner = unsafe { self.inner.as_ref() };
+        let count = inner.refcount.get();
+        inner.refcount.set(count + 1);
+        Rc {
+            inner: self.inner,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T> Drop for Rc<T> {
+    fn drop(&mut self) {
+        let inner = unsafe { self.inner.as_ref() };
+        let count = inner.refcount.get();
+        if count == 1 {
+            drop(inner);
+            let _ = unsafe { Box::from_raw(self.inner.as_ptr()) };
+        } else {
+            inner.refcount.set(count - 1);
+        }
+    }
+}
